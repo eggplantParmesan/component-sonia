a19a63002b289184a6d35f0752a5a10f7bf746b4
Neda Rohani <sonia@Nedas-MacBook-Pro.local>
2018-09-18 15:24:35 -0700

    modify config.yml

--- client/src/tests/PzElement.test.js
+++ client/src/tests/PzElement.test.js
@@ -2,9 +2,12 @@
 // import { shallow } from 'enzyme';
 // import PzElement from '../PzElement.jsx';
 
+// const items = {
+//   id: 4, productName: 'Sleek Fresh Tuna', productDescription: 'Enim odio vitae. Vel dignissimos omnis cum aut quibusdam.', color: 'yellow', price: 231, imageURL: 'https://s3-us-west-1.amazonaws.com/pictures-hrsf/download+(7).jpeg', rating: 4, reviewNumber: 186, isPrime: 1,
+// };
 // describe('PzElement', () => {
 //   test('it has method isPrime', () => {
-//     const wrapper = shallow(<PzElement />);
+//     const wrapper = shallow(<PzElement item={items}/>);
 //     expect(typeof wrapper.instance().isPrime).toBe('function');
 //   });
 //   test('it has method starMaker', () => {
